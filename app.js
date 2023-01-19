2870409f7315397948e9dabd5f888732d52be4c3
Stephen Axtell <axtell.stephen@gmail.com>
2023-01-19 17:59:42 +0200

    working with the DOM, adding and traversing. Plus exercise

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 console.dir(document);
 
-document.body.children[1].children[0].href = "http://www.youtube.com";
+// document.body.children[1].children[0].href = "http://www.youtube.com";
+
+let anchorEl = document.getElementById("external-link");
+anchorEl.href = "https://io9.com";
+
+anchorEl = document.querySelector("a");
+anchorEl.href = "https://www.udemy.com";
+
+// Add an element
+// 1. Create new element
+let newAnchor = document.createElement("a");
+newAnchor.href = "https://www.google.com";
+newAnchor.textContent = "Try Google";
+
+//2) Get access to parent to hold new element
+let firstParagraph = document.querySelector("p");
+
+//3) Insert new element into parent element content
+firstParagraph.append(newAnchor);
